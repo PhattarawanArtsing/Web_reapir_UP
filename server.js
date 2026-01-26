2baa8bba5e254bbfab94ccb26ca025769cb7cf50
kimuz57 <Ma7012015@gmail.com>
2026-01-27 00:53:10 +0700

    fix db password variable

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname));
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
